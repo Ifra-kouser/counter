ab52c9788bb2ab39b9d1501133f3829956d44058
ifravlog <mohammediifra354@gmail.com>
2022-05-01 13:04:18 +0530

    fourth commit

---
+++ index9.js
@@ -0,0 +1,23 @@
+const counter = document.getElementById("counter");
+const incrementBtn = document.getElementById("increment");
+const resetBtn = document.getElementById("reset");
+const decrementBtn = document.getElementById("deccrement");
+
+let counterValue = 0;
+incrementBtn.addEventListener("click",function (event) {
+    counterValue += 1;
+counter.innerHTML = counterValue;
+
+});
+decrementBtn.addEventListener("click",function (event) {
+    counterValue -= 1;
+counter.innerHTML = counterValue;
+
+});
+resetBtn.addEventListener("click",function (event) {
+    counterValue = 0;
+counter.innerHTML = counterValue;
+    
+});
+
+
